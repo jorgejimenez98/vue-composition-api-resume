93e8569d355f371922af4c1b16526e9d07783775
Jorge Jimenez <georgeguitarra07@gmail.com>
2023-08-26 21:31:25 +0200

    feat(home): show ref vs reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,56 @@
 <template>
-    <h1>Home</h1>
+  <h1>Reactive VS Ref</h1>
+
+  <h3>{{ ironman }}</h3>
+  <h3>{{ hulk }}</h3>
+
+  <h3>Ironam {{ ironamIsAlive }}</h3>
+  <h3>Hulk {{ hulkIsAlive }}</h3>
+
+  <div>
+    <button @click="changeIronman">Cambiar Ironman</button>
+    <button @click="changeHulk">Cambiar Hulk</button>
+    <button @click="changeStatus">Cambiar Status</button>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'home-view'
-    }
+import { ref, reactive } from "vue";
+
+export default {
+  name: "home-view",
+  setup() {
+    const ironman = ref({ name: "Tony", age: 50 });
+    const hulk = reactive({ name: "Bruce", age: 50 });
+
+    // Reactive is only for numbers and strings, is read-only
+    const ironamIsAlive = reactive(false);
+    const hulkIsAlive = ref(false);
+
+    const changeStatus = () => {
+      hulkIsAlive.value = !hulkIsAlive.value;
+    };
+
+    return {
+      ironman,
+      hulk,
+
+      ironamIsAlive,
+      hulkIsAlive,
+
+      changeStatus,
+      changeIronman: () => {
+        ironman.value.name = "Tony Stark";
+        ironman.value.age = 55;
+      },
+
+      changeHulk: () => {
+        hulk.name = "Bruce Banner";
+        hulk.age = 55;
+      },
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
