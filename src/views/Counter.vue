ef49ad8af6f926b8a95449c6d3ccc1777d3fab7b
Jorge Jimenez <georgeguitarra07@gmail.com>
2023-08-26 21:08:02 +0200

    feat(counter): add setup

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -1,14 +1,36 @@
-<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <h1>Counter View</h1>
+  <h1>Counter View</h1>
+
+  <h3>{{ counter }}</h3>
+
+  <div>
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'counter-view'
-    }
+import { ref } from "vue";
+
+export default {
+  name: "counter-view",
+  props: {},
+  emits: [],
+
+  setup() {
+    const counter = ref(5);
+
+    return {
+      // Data
+      counter,
+
+      // Methods
+      increase: () => counter.value++,
+      decrease: () => counter.value--
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
